1c13f18b991d3c18adb2775982cdecbcba2c4775
Furaha Saleh Benedict <benefatia@gmail.com>
2020-06-06 20:46:26 +0300

    Furaha HNG-02558 script added

---
+++ scripts/Furaha.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Furaha Saleh Benedict";
+$HNG_ID = "HNG-02558";
+$email = "benefatia@gmail.com";
+$language = "PHP";
+
+echo "Hello world, this is $name with HNGi7 ID $HNG_ID and email $email using $language for stage 2 task";
+?>
